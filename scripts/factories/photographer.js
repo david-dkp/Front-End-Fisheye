bd6bf2fdec78c6f0bbc37e07128d1947589cb40f
David <david.dekeuwer@gmail.com>
2022-05-30 10:45:04 +0200

    feat: create userCardDom elements with loaded datas

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,38 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/${portrait}`
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        const articleElement = document.createElement("article")
+        articleElement.classList.add("photographer-card")
+
+        const profileImageElement = document.createElement("img")
+        profileImageElement.classList.add("photographer-photo")
+        profileImageElement.setAttribute("src", picture)
+        profileImageElement.setAttribute("alt", name)
+
+        const nameElement = document.createElement("h2")
+        nameElement.classList.add("photographer-name")
+        nameElement.textContent = name
+
+        const locationElement = document.createElement("h3")
+        locationElement.classList.add("photographer-location")
+        locationElement.textContent = `${city}, ${country}`
+
+        const taglineElement = document.createElement("p")
+        taglineElement.textContent = tagline
+
+        const priceElement = document.createElement("p")
+        locationElement.classList.add("photographer-price")
+        priceElement.textContent = `${price}/jour`
+
+        articleElement.appendChild(profileImageElement)
+        articleElement.appendChild(nameElement)
+        articleElement.appendChild(locationElement)
+        articleElement.appendChild(taglineElement)
+        articleElement.appendChild(priceElement)
+        return articleElement
     }
     return { name, picture, getUserCardDOM }
 }
